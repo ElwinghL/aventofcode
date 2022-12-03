ed4557ce2a74799959f6c6beb1a53ea53c986d8b
ElwinghL <clement_1996@hotmail.com>
2022-12-03 15:41:16 +0100

    Fix function ordre after a refactoring

--- 2022/1/code.py
+++ 2022/1/code.py
@@ -8,9 +8,6 @@ def solver1():
 def solver2():
     return sumUp(mySort(list(map(lambda x: sumUp(x), getDatas())), True)[0:3])
 
-print (solver1())
-print (solver2())
-
 """
 
  888b     d888                                      888    888                    888
@@ -39,3 +36,22 @@ def sumUpListofList(l, sum = 0):
 def mySort(l, isReversed = False):
     l.sort(reverse = isReversed)
     return l
+
+"""
+
+ 888                                                      888 888             888
+ 888                                                      888 888             888
+ 888                                                      888 888             888
+ 888      .d88b.  .d8888b       888d888 .d8888b  888  888 888 888888  8888b.  888888 .d8888b
+ 888     d8P  Y8b 88K           888P"   88K      888  888 888 888        "88b 888    88K
+ 888     88888888 "Y8888b.      888     "Y8888b. 888  888 888 888    .d888888 888    "Y8888b.
+ 888     Y8b.          X88      888          X88 Y88b 888 888 Y88b.  888  888 Y88b.       X88
+ 88888888 "Y8888   88888P'      888      88888P'  "Y88888 888  "Y888 "Y888888  "Y888  88888P'
+
+
+
+
+"""
+
+print (solver1())
+print (solver2())
