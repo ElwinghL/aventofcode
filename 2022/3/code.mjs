5f62016007c93a677b626de26f7276d71602b943
ElwinghL <clement_1996@hotmail.com>
2022-12-03 06:57:26 +0100

    Solution for puzzle 3 part 2

--- 2022/3/code.mjs
+++ 2022/3/code.mjs
@@ -54,3 +54,38 @@ function solver1() {
   return score;
 }
 console.warn(solver1());
+/**
+ * https://adventofcode.com/2022/day/3#part2
+ * Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?
+ */
+function solver2() {
+  const backpacks = commonGround().split("\r\n"); //We split when there is a linebreak
+
+  const score = recurseCount(0, backpacks);
+  return score;
+}
+/**
+ * Count the sum of the priorityToken of each 3 elves group
+ * @param {Number} count The iterative count
+ * @param {Array[String]} list The backpacks
+ */
+function recurseCount(count, list) {
+  if (list.length == 0) return count;
+  //We separate or list with the spread op
+  const [firstElf, secondElf, thirdElf, ...otherElves] = list;
+  for (let first = 0; first < firstElf.length; ++first) {
+    for (let second = 0; second < secondElf.length; ++second) {
+      for (let third = 0; third < thirdElf.length; ++third) {
+        //we go in our three backpacks and find the common elem, one's it's found, we just keep recursing
+        if (
+          firstElf[first] == secondElf[second] &&
+          firstElf[first] == thirdElf[third]
+        )
+          return recurseCount(count + ABCD[firstElf[first]], otherElves);
+      }
+    }
+  }
+  //For security if our loop didn't get through
+  return recurseCount(count, otherElves);
+}
+console.warn(solver2());
