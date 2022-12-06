cab52cba3c4d3313134619fcd939235d9493de2b
ElwinghL <clement_1996@hotmail.com>
2022-12-06 11:09:33 +0100

    First attempt to the second question of day 6

--- 2022/6/code.mjs
+++ 2022/6/code.mjs
@@ -34,5 +34,33 @@ console.warn(solver1());
  * https://adventofcode.com/2022/day/6#part2
  * How many characters need to be processed before the first start-of-message marker is detected?
  */
-function solver2() {}
+function solver2() {
+  const dataStream = commonGround();
+  let [a, b, c, d, e, f, g, h, i, j, k, l, m, n, ...rest] =
+    dataStream.split("");
+  let set = new Set([a, b, c, d, e, f, g, h, i, j, k, l, m, n]);
+  while (set.size < 14) {
+    [a, b, c, d, e, f, g, h, i, j, k, l, m, n, ...rest] = [
+      b,
+      c,
+      d,
+      e,
+      f,
+      g,
+      h,
+      i,
+      j,
+      k,
+      l,
+      m,
+      n,
+      ...rest,
+    ];
+    set = new Set([a, b, c, d, e, f, g, h, i, j, k, l, m, n]);
+  }
+
+  return (
+    dataStream.indexOf([a, b, c, d, e, f, g, h, i, j, k, l, m, n].join("")) + 14
+  );
+}
 console.warn(solver2());
